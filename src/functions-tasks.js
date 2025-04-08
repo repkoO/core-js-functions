39b40d1eeda903f46ed1443ed6345665ca91d984
Kirill Medvedev <repkomail@gmail.com>
2025-04-08 23:30:06 +0200

    feat: solve getPolynom

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -90,8 +90,18 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (args.length === 0) return null;
+
+  return function funcName(x) {
+    let result = 0;
+
+    for (let i = 0; i < args.length; i += 1) {
+      result += args[i] * x ** (args.length - 1 - i);
+    }
+
+    return result;
+  };
 }
 
 /**
